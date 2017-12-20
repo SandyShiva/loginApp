19d214c20e6da7e361cc5bb0d5f51813855119ea
SandyShiva <shivaprasadramagiri@gmail.com>
2017-12-20 19:53:16 +0530

    Changed by me
    Changed by me

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,7 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  iontitle: any = 'HOME';
   constructor(public navCtrl: NavController) {
 
   }
